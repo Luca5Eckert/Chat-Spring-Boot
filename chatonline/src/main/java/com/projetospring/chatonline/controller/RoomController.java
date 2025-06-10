e00171a74df017ba3d1451f21851cb093d046bd1
lucas_eckert <lucas_eckert@SN-360459.edu.fiesc.com.br>
2025-06-09 21:51:42 -0300

    CHAT-1 fix: arrumado controller modelo

--- chatonline/src/main/java/com/projetospring/chatonline/controller/RoomController.java
+++ chatonline/src/main/java/com/projetospring/chatonline/controller/RoomController.java
@@ -1,8 +1,8 @@
 package com.projetospring.chatonline.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoomController {
 	
 }
--- chatonline/src/main/java/com/projetospring/chatonline/controller/UserController.java
+++ chatonline/src/main/java/com/projetospring/chatonline/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.projetospring.chatonline.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
 }
