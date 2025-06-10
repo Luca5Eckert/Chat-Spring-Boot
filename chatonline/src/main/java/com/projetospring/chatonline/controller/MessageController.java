3b822259f28582398e18c61a41212da1f4f014f8
lucas_eckert <lucas_eckert@SN-360459.edu.fiesc.com.br>
2025-06-09 21:49:59 -0300

    CHAT-1 update: adiciona dto para o modelo

--- chatonline/src/main/java/com/projetospring/chatonline/controller/MessageController.java
+++ chatonline/src/main/java/com/projetospring/chatonline/controller/MessageController.java
@@ -1,5 +1,17 @@
 package com.projetospring.chatonline.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+import com.projetospring.chatonline.repository.MessageRepository;
+
+@Controller
 public class MessageController {
 
+	@Autowired
+	private MessageRepository messageRepository;
+	
+	
 }
