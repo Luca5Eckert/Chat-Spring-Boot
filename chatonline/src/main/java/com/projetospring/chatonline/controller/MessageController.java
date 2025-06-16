a104f68f6c16f7c88b77514ddeae8c8e0cc3ebd0
LucasEckert <ecklukatv@gmail.com>
2025-06-15 23:23:55 -0300

    feat(websocket): Configure initial Spring WebSocket support
    This commit sets up the basic WebSocket message broker for real-time
    communication. It includes enabling simple brokers for '/topic' and
    '/queue' and setting '/app' as the application destination prefix. The
    '/ws' STOMP endpoint is also registered with SockJS support. The
    `MessageController` is included to handle messages via
    `@MessageMapping`.
    
    Refs: CHAT-2, CHAT-5

--- chatonline/src/main/java/com/projetospring/chatonline/controller/MessageController.java
+++ chatonline/src/main/java/com/projetospring/chatonline/controller/MessageController.java
@@ -1,9 +1,26 @@
 package com.projetospring.chatonline.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projetospring.chatonline.dtos.MessageDto;
+import com.projetospring.chatonline.service.SendMessageCase;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class MessageController {
 
-	
+	@Autowired
+	private SendMessageCase sendMessageCase;
+
+	@MessageMapping("/api/chat/{room_id}/sendMessage")
+	public ResponseEntity<?> sendMessage(@RequestBody @Valid MessageDto message) {
+		sendMessageCase.execute(message);
+		return ResponseEntity.accepted().body("Message sent successfully");
+	}
+
 }
